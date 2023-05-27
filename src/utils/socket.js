f73b321e919f7abd77a5ffb86a91bf5b173ed588
d3xferno <53827229+d3xferno@users.noreply.github.com>
2023-05-27 20:12:43 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,11 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://chess-server-92na.onrender.com';
 
 export const socket = io(URL,{
     withCredentials: true,
     extraHeaders: {
-        'Access-Control-Allow-Origin': "http://localhost:5173",
+        'Access-Control-Allow-Origin': "https://chess-kage.vercel.app/",
         'Access-Control-Allow-Methods': 'GET, POST',
         'Access-Control-Allow-Credentials': true,
     },
