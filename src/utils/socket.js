ef99c6ce3d0be120912d56c75da965cb85497c19
d3xferno <53827229+d3xferno@users.noreply.github.com>
2023-05-28 11:11:47 +0530

    change frontend url - socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,7 +5,7 @@ const URL = 'https://chess-server-92na.onrender.com'
 export const socket = io(URL,{
     withCredentials: true,
     extraHeaders: {
-        'Access-Control-Allow-Origin': "https://chess-shajith-git-main-d3xferno.vercel.app/",
+        'Access-Control-Allow-Origin': "https://chess-kage-git-main-d3xferno.vercel.app/",
         'Access-Control-Allow-Methods': 'GET, POST',
         'Access-Control-Allow-Credentials': true,
     },
