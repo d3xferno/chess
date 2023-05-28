296fe2cf18d925c4ae29f675c120215885ba4705
d3xferno <53827229+d3xferno@users.noreply.github.com>
2023-05-28 11:16:49 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,6 @@ import { io } from 'socket.io-client';
 const URL = 'https://chess-server-92na.onrender.com'
 
 export const socket = io(URL,{
-    withCredentials: true,
     extraHeaders: {
         'Access-Control-Allow-Origin': "https://chess-kage-git-main-d3xferno.vercel.app/",
         'Access-Control-Allow-Methods': 'GET, POST',
